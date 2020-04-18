985cfc5e6625cb0c3f8de2d35d161edd198433ae
Nathaniel Brown <nateabrown@icloud.com>
2020-04-18 17:04:11 -0500

    fix things

--- lambda/handlers/disciplinary_points_intent.js
+++ lambda/handlers/disciplinary_points_intent.js
@@ -4,7 +4,7 @@ function DisciplinaryPointsIntent(handlerInput) {
     let speakOutput = "The speak output has not been modified."; 
     
     let infractionSpecified = handlerInput.requestEnvelope.request.intent.slots.infraction.resolutions.resolutionsPerAuthority[0].values;
-    console.log(`Empty resolutions looks like this: ${infractionSpecified} and has these keys: ${Object.keys(infractionSpecified)}`);
+    console.log(`Empty resolutions looks like this: ${infractionSpecified}`);
     if (infractionSpecified == null || infractionSpecified === undefined) {
         return handlerInput.responseBuilder
         .speak("Sorry, I had trouble understanding the violation requested. Perhaps rephrase it.")
