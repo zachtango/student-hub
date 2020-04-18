f86ef2536fa574ad857e1e760d44438c7c5a8b41
Nathaniel Brown <nateabrown@icloud.com>
2020-04-18 13:46:41 -0500

    Discovery intent slot handling

--- lambda/handlers/discovery_intent.js
+++ lambda/handlers/discovery_intent.js
@@ -4,16 +4,17 @@ function DiscoveryIntent(handlerInput) {
     let speakOutput = "Speak output is not modified in Discovery Intent. ";
     let speakAddition = ""; // Use this to add any additional content you want Alexa to say
 
-    // Possible IDs for bus_location: {main, d_park}
-    let location = String(handlerInput.requestEnvelope.request.intent.slots.bus_location.resolutions.resolutionsPerAuthority[0].values[0].value.id);
-    if (location == null) {
-        console.log(`Location was ${location}, setting it to Main Campus`);
+    let isLocationSpecified = handlerInput.requestEnvelope.request.intent.slots.bus_location.resolutions;
+    let location;
+    if (isLocationSpecified == null) {
+        console.log(`Location was not specified, setting it to Main Campus`);
         location = 'main';
         speakAddition += "For future reference, you can specify whether you want times from Main Campus or from Discovery Park. ";
     } else {
+        location = String(handlerInput.requestEnvelope.request.intent.slots.bus_location.resolutions.resolutionsPerAuthority[0].values[0].value.id)
         console.log(`\nGot location from resolutions: ${location}`);
     }
-    let time = String(handlerInput.requestEnvelope.request.intent.slots.time.value);
+    let time = handlerInput.requestEnvelope.request.intent.slots.time.value;
     if (time == null) {
         console.log(`Time was ${time}, setting it to PRESENT_REF`);
         time = 'PRESENT_REF'; // set time to the current time, according to the Amazon time format
