a21b7ad1c235d2ee30fc217da6bd10e86f1717bd
Nathaniel Brown <nateabrown@icloud.com>
2020-04-18 17:42:47 -0500

    Add debugging

--- lambda/handlers/level_x_violation_intent.js
+++ lambda/handlers/level_x_violation_intent.js
@@ -7,6 +7,7 @@ function LevelXViolationIntent(handlerInput) {
     let speakOutput = "Speak output has not been modified.";
 
     let level = handlerInput.requestEnvelope.request.intent.slots.level.value;
+    console.log(`The level we got was: ${level}`);
     if (!ACCEPTED_LEVELS.includes(level)) {
         return handlerInput.responseBuilder
         .speak("Please specify a value between 1 and 5.")
