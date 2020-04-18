d10198f5e47dbabdb6861bb73b91e8fa05e26190
Nathaniel Brown <nateabrown@icloud.com>
2020-04-17 21:09:48 -0500

    add documentation

--- lambda/index.js
+++ lambda/index.js
@@ -15,19 +15,7 @@ const LaunchRequestHandler = {
             .getResponse();
     }
 };
-// const HelloWorldIntentHandler = {
-//     canHandle(handlerInput) {
-//         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-//             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
-//     },
-//     handle(handlerInput) {
-//         const speakOutput = 'Hello World!';
-//         return handlerInput.responseBuilder
-//             .speak(speakOutput)
-//             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-//             .getResponse();
-//     }
-// };
+
 const HelpIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
@@ -56,6 +44,9 @@ const CancelAndStopIntentHandler = {
     }
 };
 
+/**
+ * This is where we sort our intents that are stored in separate files.
+ */
 const IntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
