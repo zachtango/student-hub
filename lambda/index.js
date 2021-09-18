7b3859c9116a10323799e3de3734212688d4ca8e
zach <zachtangoj@gmail.com>
2021-09-18 00:21:16 -0500

    dot env config

--- lambda/index.js
+++ lambda/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 // This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
 // Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
 // session persistence, api calls, and more.
